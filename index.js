99ee47d16d6d5f4eb90d5e9c3cec3701da5000f9
I_4m_SilverGWolf <ssoto4383@outlook.com>
2025-05-08 20:10:28 -0600

    Modificado de Index para poder ejecutar en render

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 var app = express();
-var fire = require('./fire')
 var cors = require('cors');
 var bodyParser = require('body-parser');
 app.use(cors());
